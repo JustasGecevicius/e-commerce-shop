6a9bd8b18c5838a4cf08b3b326cc060874f6b7b1
JustasGecevicius <justas.gece@gmail.com>
2025-11-09 00:38:49 +0200

    test workflows GITHUB_TOKEN6

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -1,21 +1,15 @@
 import { NavigationButton } from "../Components/NavigationButton";
 import { ShoppingCartSymbol } from "../Components/ShoppingCartSymbol";
 
-
 export const Header = ({ cartCount, logo, cartLogo }) => {
-
-
   return (
     <div className="header">
       <div className="websiteName">
         <img className="logoImage" alt="logo" src={logo}></img>
       </div>
       <div className="navButtons">
-        <NavigationButton page="/e-commerce-shop" pageName="Home" />
-        <NavigationButton
-          page="/e-commerce-shop/catalogue"
-          pageName="Catalogue"
-        />
+        {/* <NavigationButton page="/e-commerce-shop" pageName="Home" /> */}
+        <NavigationButton page="/e-commerce-shop/catalogue" pageName="Catalogue" />
         <NavigationButton page="/e-commerce-shop/profile" pageName="Profile" />
         <ShoppingCartSymbol
           page="/e-commerce-shop/shopping%20cart"
